2d89a787fe94236086403702287ca224994a8eca
GitHub Action <action@github.com>
2022-10-11 06:26:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/sxuns/mycodegenerator/MyBundle.kt
+++ src/main/kotlin/com/github/sxuns/mycodegenerator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sxuns.mycodegenerator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sxuns/mycodegenerator/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sxuns/mycodegenerator/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sxuns.mycodegenerator.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sxuns.mycodegenerator.MyBundle
 
 class MyApplicationService {
 
